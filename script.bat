01a06288bd86d04a5938cd0801a8656a0984aba5
andrew <63661640+that-exploiter@users.noreply.github.com>
2024-10-18 00:16:46 +0300

    added a new pass (a special one) & bugfixes

--- script.bat
+++ script.bat
@@ -4,7 +4,7 @@ cls
 title jumper
 setlocal enabledelayedexpansion
 
-rem Define your passwords
+rem premium passwords getting sucked off a cliff (idk)
 set "pass1=thisniggasucksdude"
 set "pass2=niggersinparis"
 set "pass3=enesisiktim"
@@ -12,25 +12,48 @@ set "pass4=usecodebuttersmoothfor10percentoffyourvapeclientpurchase"
 set "pass5=lockntdeveloper"
 set "pass6=mtakusucksass"
 set "pass7=iosys"
-set "pass8=alanisactuallypronouncedasanal"
+set "special=alanisactuallypronouncedasanal"
 
-rem Kill explorer.exe
+rem killing explorer exe for security purposes (to not get cracked by skids)
 taskkill /f /im explorer.exe >nul
 
-echo If you don't have premium, just press Enter..
+:key
+cls
+
+rem input
+echo If you don't own a premium subscription, just press Enter..
 echo.
 set /p "input=Type the key if you have premium: "
 
-rem Initialize a flag to check if a valid password was found
+rem too lazy to type what it does
 set "validPass=0"
 
-rem Check if the input matches any of the passwords
-for %%p in ("%pass1%" "%pass2%" "%pass3%" "%pass4%" "%pass5%" "%pass6%" "%pass7%" "%pass8%") do (
-    if "!input!"=="%%~p" (
-        set "validPass=1"
-    )
+rem checking if the input matches the "special" password
+if "!input!"=="!special!" (
+    set "validPass=3"
 )
 
+rem checking if the input matches one of the premium passwords
+if "!input!"=="!pass1!" set "validPass=1"
+if "!input!"=="!pass2!" set "validPass=1"
+if "!input!"=="!pass3!" set "validPass=1"
+if "!input!"=="!pass4!" set "validPass=1"
+if "!input!"=="!pass5!" set "validPass=1"
+if "!input!"=="!pass6!" set "validPass=1"
+if "!input!"=="!pass7!" set "validPass=1"
+
+rem discord server
+if "!validPass!"=="3" (
+    cls
+    echo you are invited to our discord server..
+    echo discord.gg/kNywru4Z7v
+    echo have fun!
+    pause
+    timeout 2 >nul
+    goto :key
+)
+
+rem premium pass tier
 if "!validPass!"=="1" (
     cls
     powershell /c taskkill /f /im etkontrol.exe >nul
@@ -45,9 +68,13 @@ if "!validPass!"=="1" (
     netsh interface ip set dns "Wi-Fi" static 1.1.1.1 primary >nul
     netsh interface ip set dns "Ethernet" static 1.1.1.1 primary >nul
     echo done.
-    echo and thank you so so much for supporting the project!
+    echo DISCLAIMER: do not close the powershell window.
+    timeout 5 >nul
+    cls
+    echo thank you so so much for supporting the project!
     start powershell /c timeout 175 >nul && netsh interface ip set dns "Wi-Fi" static 192.168.1.1 primary >nul && netsh interface ip set dns "Ethernet" static 192.168.1.1 primary >nul
 ) else (
+    rem Handle the free tier (empty input or invalid password)
     cls
     powershell /c taskkill /f /im etkontrol.exe >nul
     timeout 1 >nul
